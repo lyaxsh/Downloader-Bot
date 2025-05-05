662426a2fa6e5d984bc43d6f5f8c61fe57a31733
Laksh Nahar <laksh14008@gmail.com>
2025-05-05 19:17:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,16 @@ custom_api_url = str(os.getenv("custom_api_url"))
 MEASUREMENT_ID = str(os.getenv("MEASUREMENT_ID"))
 API_SECRET = str(os.getenv("API_SECRET"))
 CHANNEL_ID = str(os.getenv("CHANNEL_ID"))
-CHANNEL_USERNAME = str(os.getenv("CHANNEL_USERNAME"))
 OUTPUT_DIR = "downloads"
 INSTAGRAM_RAPID_API_HOST = str(os.getenv("INSTAGRAM_RAPID_API_HOST"))
 INSTAGRAM_RAPID_API_KEY1 = str(os.getenv("INSTAGRAM_RAPID_API_KEY1"))
 INSTAGRAM_RAPID_API_KEY2 = str(os.getenv("INSTAGRAM_RAPID_API_KEY2"))
 
 BOT_COMMANDS = [
-    {'command': 'start', 'description': '🚀Початок роботи / Get started🔥'},
-    {'command': 'settings', 'description': '⚙️Налаштування / Settings🛠'},
-    {'command': 'stats', 'description': '📊Статистика / Statistics📈'},
-    {'command': 'remove_keyboard', 'description': '🗑️Прибрати клавіатуру / Remove keyboard🗑️'},
+    {'command': 'start', 'description': '🚀 Get started🔥'},
+    {'command': 'settings', 'description': '⚙️ Settings🛠'},
+    {'command': 'stats', 'description': '📊 Statistics📈'},
+    {'command': 'remove_keyboard', 'description': '🗑️ Remove keyboard🗑️'},
 
 ]
 
